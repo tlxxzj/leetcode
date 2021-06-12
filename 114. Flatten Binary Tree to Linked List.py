942604bcabf805ac6a7480a0c284e298f05af9b4
MATATA <tlxxzj@qq.com>
2021-06-12 20:50:22 +0800

    114. Flatten Binary Tree to Linked List

--- 114. Flatten Binary Tree to Linked List.py
+++ 114. Flatten Binary Tree to Linked List.py
@@ -1,36 +1,28 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution(object):
-    def flatten(self, root):
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: TreeNode) -> None:
         """
-        :type root: TreeNode
-        :rtype: void Do not return anything, modify root in-place instead.
-        """# Definition for a binary tree node.
-        def flatten(root):
-            nil = None
-            if root == nil: return [None, None]
-            ret = [root, root]
-            l = flatten(root.left)
-            r = flatten(root.right)
-            root.left = root.right = nil
-            if l[0] == nil and r[0] == nil:
-                pass
-            elif l[0] == nil:
-                root.right, r[0].left = r[0], nil
-                ret[1] = r[1]
-            elif r[0] == nil:
-                root.right, l[0].left = l[0], nil
-                ret[1] = l[1]
-            else:
-                root.right, l[0].left = l[0], nil   
-                l[1].right, r[0].left = r[0], nil
-                ret[1] = r[1]
-            return ret
-        root = flatten(root)
-        
-        
+        Do not return anything, modify root in-place instead.
+        """
+
+        def preOrderTraversal(node):
+            if not node:
+                return None
+            left, right = node.left, node.right
+            node.left = None
+            node.right = left
+            if left:
+                node = preOrderTraversal(left)
+            node.right = right
+            if right:
+                node = preOrderTraversal(right)
+            return node
+
+        preOrderTraversal(root)
+
+
