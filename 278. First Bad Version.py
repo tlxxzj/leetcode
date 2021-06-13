b1a84de9131377e36b4aebecb94c159e1be9e68f
MATATA <tlxxzj@qq.com>
2021-06-13 09:19:45 +0800

    278. First Bad Version

--- 278. First Bad Version.py
+++ 278. First Bad Version.py
@@ -1,22 +1,19 @@
 # The isBadVersion API is already defined for you.
 # @param version, an integer
-# @return a bool
+# @return an integer
 # def isBadVersion(version):
 
-class Solution(object):
+class Solution:
     def firstBadVersion(self, n):
         """
         :type n: int
         :rtype: int
         """
-        l,r,ret=1,n,1
-        while l<=r:
-            m=(l+r)/2
-            if isBadVersion(m):
-                r=m-1
-                ret=m
+        l, r = 1, n
+        while l <= r:
+            m = (l + r) >> 1
+            if not isBadVersion(m):
+                l = m + 1
             else:
-                l=m+1
-        return ret
-        
-        
+                r = m - 1
+        return l
