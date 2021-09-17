eb4c4a2559593bf512ced5555029f512c0f0bae3
MATATA <tlxxzj@qq.com>
2021-09-17 10:30:25 +0800

    900. RLE Iterator

--- 900. RLE Iterator.py
+++ 900. RLE Iterator.py
@@ -1,26 +1,32 @@
 class RLEIterator:
 
-    def __init__(self, A: List[int]):
-        A.reverse()
-        self.a = A
-        self.x = 0
-        self.y = 0
+    def __init__(self, encoding: List[int]):
+        self.encoding = encoding
+        self.i = 0
+        self.l = len(self.encoding)
+        self.val = 0
+        self.t = 0
 
     def next(self, n: int) -> int:
-        ret = -1
+        val = -1
         while n > 0:
-            if self.x == 0:
-                if len(self.a) > 0:
-                    self.x, self.y = self.a.pop(), self.a.pop()
-                else:
-                    break
-            k = min(self.x, n)
-            n -= k
-            self.x -= k
-            if n == 0:
-                ret = self.y
-        return ret
+            if self.t > 0:
+                x = min(n, self.t)
+                n -= x
+                self.t -= x
+                val = self.val
+            else:
+                if self.i + 1 >= self.l:
+                    return -1
+                self.t = self.encoding[self.i]
+                self.val = self.encoding[self.i + 1]
+                self.i = self.i + 2
+        return val
+
+        
+        
+
 
 # Your RLEIterator object will be instantiated and called as such:
-# obj = RLEIterator(A)
+# obj = RLEIterator(encoding)
 # param_1 = obj.next(n)
