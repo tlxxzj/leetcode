59b028e32e2c48958ef62ad43b019a49fba1f07c
MATATA <tlxxzj@qq.com>
2021-09-18 11:00:46 +0800

    343. Integer Break

--- 343. Integer Break.py
+++ 343. Integer Break.py
@@ -1,9 +1,10 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
-        dp= [0] * (n + 1)
-        dp[1] = 1
-        dp[2] = 1
-        for i in range(3, n+1):
-            for j in range(1, i):
-                dp[i] = max(dp[i], max(j, dp[j]) * max(i-j, dp[i-j]))
-        return dp[n]
+        if n == 2: return 1
+        if n == 3: return 2
+        if n == 4: return 4
+        ret = 1
+        while n > 4:
+            ret *= 3
+            n -= 3
+        return ret * max(1, n)
