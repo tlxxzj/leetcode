fe842fc8ae4af10f85d442fa2244c3375e68ae08
MATATA <tlxxzj@qq.com>
2021-09-29 14:04:44 +0800

    791. Custom Sort String

--- 791. Custom Sort String.py
+++ 791. Custom Sort String.py
@@ -1,14 +1,3 @@
 class Solution:
-    def customSortString(self, S, T):
-        """
-        :type S: str
-        :type T: str
-        :rtype: str
-        """
-        key_map = {}
-        for i, c in enumerate(S):
-            key_map[c] = i
-        T = list(T)
-        T.sort(key=lambda x: key_map.get(x, 26))
-        return ''.join(T)
-        
+    def customSortString(self, order: str, s: str) -> str:
+        return ''.join(sorted(list(s), key=lambda x: order.index(x) if x in order else -1))
