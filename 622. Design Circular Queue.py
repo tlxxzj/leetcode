f6e31bf3e8a9167b1dec3daadbd88ff4e0432da4
MATATA <tlxxzj@qq.com>
2021-09-30 15:12:52 +0800

    622. Design Circular Queue

--- 622. Design Circular Queue.py
+++ 622. Design Circular Queue.py
@@ -1,81 +1,42 @@
 class MyCircularQueue:
 
-    def __init__(self, k):
-        """
-        Initialize your data structure here. Set the size of the queue to be k.
-        :type k: int
-        """
+    def __init__(self, k: int):
         self._data = [0] * k
         self._k = k
-        self._s = 0
+        self._size = 0
         self._f = 0
-        self._r = -1
-        
+        self._r = 0
 
-    def enQueue(self, value):
-        """
-        Insert an element into the circular queue. Return true if the operation is successful.
-        :type value: int
-        :rtype: bool
-        """
+    def enQueue(self, value: int) -> bool:
         if self.isFull():
             return False
-        
-        self._r = (self._r + 1) % self._k
+        self._size += 1
         self._data[self._r] = value
-        self._s += 1
+        self._r = (self._r + 1) % self._k
         return True
-        
-        
 
-    def deQueue(self):
-        """
-        Delete an element from the circular queue. Return true if the operation is successful.
-        :rtype: bool
-        """
+    def deQueue(self) -> bool:
         if self.isEmpty():
             return False
-        
+        self._size -= 1
         self._f = (self._f + 1) % self._k
-        self._s -= 1
         return True
-        
-        
 
-    def Front(self):
-        """
-        Get the front item from the queue.
-        :rtype: int
-        """
+    def Front(self) -> int:
         if self.isEmpty():
             return -1
         return self._data[self._f]
-        
 
-    def Rear(self):
-        """
-        Get the last item from the queue.
-        :rtype: int
-        """
+    def Rear(self) -> int:
         if self.isEmpty():
             return -1
-        return self._data[self._r]
-        
+        return self._data[self._r-1]
 
-    def isEmpty(self):
-        """
-        Checks whether the circular queue is empty or not.
-        :rtype: bool
-        """
-        return self._s == 0
-        
+    def isEmpty(self) -> bool:
+        return self._size == 0
 
-    def isFull(self):
-        """
-        Checks whether the circular queue is full or not.
-        :rtype: bool
-        """
-        return self._s == self._k
+    def isFull(self) -> bool:
+        return self._size == self._k
 
 
 # Your MyCircularQueue object will be instantiated and called as such:
