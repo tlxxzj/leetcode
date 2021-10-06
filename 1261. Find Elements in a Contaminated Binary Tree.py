92a54d124b3aacca97cdb36d5d21c96934fe9005
MATATA <tlxxzj@qq.com>
2021-10-06 16:54:49 +0800

    1261. Find Elements in a Contaminated Binary Tree

--- 1261. Find Elements in a Contaminated Binary Tree.py
+++ 1261. Find Elements in a Contaminated Binary Tree.py
@@ -1,28 +1,28 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class FindElements:
 
     def __init__(self, root: TreeNode):
-        self.nodes = set()
-        root.val = 0
-        q = [root]
-        while len(q) > 0:
-            x = q.pop()
-            self.nodes.add(x.val)
-            if x.left:
-                x.left.val = 2 * x.val + 1
-                q.append(x.left)
-            if x.right:
-                x.right.val = 2 * x.val + 2
-                q.append(x.right)
+        nodes = [root] if root else []
+        if root:
+            root.val = 0
+        self._node_set = set()
+        while len(nodes) > 0:
+            node = nodes.pop()
+            self._node_set.add(node.val)
+            if node.left:
+                node.left.val = node.val * 2 + 1
+                nodes.append(node.left)
+            if node.right:
+                node.right.val = node.val * 2 + 2
+                nodes.append(node.right)
 
     def find(self, target: int) -> bool:
-        return target in self.nodes
+        return target in self._node_set
 
 
 # Your FindElements object will be instantiated and called as such:
