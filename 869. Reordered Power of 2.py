566ba3184f919a55b16b9f07ca48e7fed09e0391
MATATA <tlxxzj@qq.com>
2021-10-13 14:00:46 +0800

    869. Reordered Power of 2

--- 869. Reordered Power of 2.py
+++ 869. Reordered Power of 2.py
@@ -1,8 +1,3 @@
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
-        p2 = set()
-        for i in range(32):
-            num = 1 << i
-            num = ''.join(sorted(list(str(num))))
-            p2.add(num)
-        return ''.join(sorted(list(str(n)))) in p2
+        return ''.join(sorted(list(str(n)))) in set([''.join(sorted(list(str(1<<i)))) for i in range(32)])
