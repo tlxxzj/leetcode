4cc80a2c7b8ac3144ebfcb2e34ed4125d05c08c4
MATATA <tlxxzj@qq.com>
2021-10-26 13:21:16 +0800

    655. Print Binary Tree

--- 655. Print Binary Tree.py
+++ 655. Print Binary Tree.py
@@ -1,34 +1,30 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution(object):
-    def printTree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[str]]
-        """
-        
-        def geth(root):
-            if not root:
-                return 0
-            return max(geth(root.left), geth(root.right)) + 1
-        
-        H = geth(root)
-        n = (2**H) - 1
-        ret = [[""] * n for i in range(H)]
-        
-        def travel(root, ret, h, l, r):
-            if not root:
-                return
-            m = (l + r) // 2
-            ret[h][m] = str(root.val)
-            travel(root.left, ret, h+1, l, m-1)
-            travel(root.right, ret, h+1, m+1, r)
-        travel(root, ret, 0, 0, n)
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def printTree(self, root: TreeNode) -> List[List[str]]:
+        def getHeight(node):
+            if not node: return 0
+            return max(getHeight(node.left), getHeight(node.right)) + 1
+        height = getHeight(root)
+        m = height
+        n = (1 << m) - 1
+        #print(m, n)
+        ret = []
+        q = [(root, 0, n)] if height > 0 else []
+        while len(q) > 0:
+            q2 = []
+            row = [''] * n
+            for node, left, right in q:
+                mid = (left + right) >> 1
+                row[mid] = str(node.val)
+                if node.left:
+                    q2.append((node.left, left, mid-1))
+                if node.right:
+                    q2.append((node.right, mid+1, right))
+            q = q2
+            ret.append(row)
         return ret
-        
-        
