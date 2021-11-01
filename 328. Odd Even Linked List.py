816224f0f50ad088d7b3972fc05167f7cbba6dab
MATATA <tlxxzj@qq.com>
2021-11-01 11:01:41 +0800

    328. Odd Even Linked List

--- 328. Odd Even Linked List.py
+++ 328. Odd Even Linked List.py
@@ -1,40 +1,19 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-class Solution(object):
-    def oddEvenList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        if (not head) or (not head.next) or (not head.next.next):
-            return head
-        
-        def iterlist(h):
-            while h:
-                x = h.next
-                yield h
-                h = x
-        
-        tail1, tail2, h2 = head, head.next, head.next
-        i = 0
-        for h in iterlist(head.next.next):
-            print h.val
-            i += 1
-            if i&1:
-                tail1.next = h
-                tail1 = h
-            else:
-                tail2.next = h
-                tail2 = h
-        tail1.next = h2
-        tail2.next = None
-        
-        return head
-            
-        
-        
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if (not head) or (not head.next): return head
+        r1, r2 = ListNode(0), ListNode(0)
+        h1, h2 = r1, r2
+        while head:
+            h1.next = head
+            h2.next = head.next
+            h1 = h1.next
+            h2 = h2.next
+            head = head.next.next if head.next else None
+        h1.next = r2.next
+        return r1.next
         
