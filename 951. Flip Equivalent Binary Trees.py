d06ef45e46f7814dafe76013be3901c3c3cb771a
MATATA <tlxxzj@qq.com>
2021-11-17 15:42:46 +0800

    951. Flip Equivalent Binary Trees

--- 951. Flip Equivalent Binary Trees.py
+++ 951. Flip Equivalent Binary Trees.py
@@ -1,24 +1,18 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution(object):
-    def flipEquiv(self, root1, root2):
-        """
-        :type root1: TreeNode
-        :type root2: TreeNode
-        :rtype: bool
-        """
-        if root1 is None and root2 is None:
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flipEquiv(self, root1: TreeNode, root2: TreeNode) -> bool:
+        def getVal(node):
+            if node:
+                return node.val
+            return None   
+        if getVal(root1) != getVal(root2):
+            return False
+        if not root1:
             return True
-        if root1 is None or root2 is None:
-            return False
-        if root1.val != root2.val:
-            return False
-        f1 = self.flipEquiv(root1.left, root2.left) and self.flipEquiv(root1.right, root2.right)
-        f2 = self.flipEquiv(root1.left, root2.right) and self.flipEquiv(root1.right, root2.left)
-        return f1 or f2
-        
+        l1, r1, l2, r2 = root1.left, root1.right, root2.left, root2.right
+        return (self.flipEquiv(l1, l2) and self.flipEquiv(r1, r2)) or(self.flipEquiv(l1, r2) and self.flipEquiv(r1, l2))
