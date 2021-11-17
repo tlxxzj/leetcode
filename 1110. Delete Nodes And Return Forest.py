07cdff205a1faf19f9fabc7a461bc31cc72aa20b
MATATA <tlxxzj@qq.com>
2021-11-17 16:13:25 +0800

    1110. Delete Nodes And Return Forest

--- 1110. Delete Nodes And Return Forest.py
+++ 1110. Delete Nodes And Return Forest.py
@@ -1,31 +1,24 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def delNodes(self, root: TreeNode, to_delete: List[int]) -> List[TreeNode]:
-        ret = []
-        dels = set(to_delete)
-        q = [root]
-        if root.val not in dels:
-            ret.append(root)
-        while len(q) > 0:
-            node = q.pop()
-            if node.left:
-                q.append(node.left)
-            if node.right:
-                q.append(node.right)
-            if node.val in dels:
-                if node.left and node.left.val not in dels:
+        to_delete = set(to_delete)
+        ret = [] if root.val in to_delete else [root]
+        def getForest(node):
+            if not node:
+                return None
+            node.left = getForest(node.left)
+            node.right = getForest(node.right)
+            if node.val in to_delete:
+                if node.left and (node.left not in to_delete):
                     ret.append(node.left)
-                if node.right and node.right.val not in dels:
+                if node.right and (node.right not in to_delete):
                     ret.append(node.right)
-            if node.left and node.left.val in dels:
-                node.left = None
-            if node.right and node.right.val in dels:
-                node.right = None
-            
+                return None
+            return node
+        getForest(root)
         return ret
