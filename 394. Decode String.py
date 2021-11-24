f86b57916bd988f7d7aac05ac37335ec2fb5f6d1
MATATA <tlxxzj@qq.com>
2021-11-24 10:05:22 +0800

    394. Decode String

--- 394. Decode String.py
+++ 394. Decode String.py
@@ -1,50 +1,32 @@
-class Solution(object):
-    def decodeString(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        st = [0, len(s)]
-        def next_token():
-            if st[0] >= st[1]:
-                return ['eof', 0]
-            elif s[st[0]].isdigit():
-                begin = st[0]
-                while st[0] < st[1] and s[st[0]].isdigit():
-                    st[0] += 1
-                return ['int', int(s[begin: st[0]])]
-            elif s[st[0]].isalpha():
-                begin = st[0]
-                while st[0] < st[1] and s[st[0]].isalpha():
-                    st[0] += 1
-                return ['str', s[begin: st[0]]]
+class Solution:
+    def decodeString(self, s: str) -> str:
+        n = len(s)
+        q = []
+        i = 0
+        while i < n:
+            if s[i].isdigit():
+                j = i + 1
+                while s[j].isdigit():
+                    j += 1
+                q.append(int(s[i:j]))
+                i = j
+            elif s[i] == '[':
+                q.append('[')
+                i += 1
+            elif s[i] == ']':
+                i += 1
+                x = ''
+                while q[-1] != '[':
+                    x = q.pop() + x
+                q.pop()
+                x = q.pop() * x
+                q.append(x)
             else:
-                st[0] += 1
-                return ['[]', 0]
-        '''
-        while 1:
-            tok = next_token()
-            print tok
-            if tok[0] == 'eof':
-                break
-        '''
-        
-        def decode(e):
-            tok = next_token()
-            r = ''
-            while tok[0] != e:
-                if tok[0] == 'int':
-                    t = tok[1]
-                    next_token() # '['
-                    s = decode('[]')
-                    r += s*t
-                elif tok[0] =='str':
-                    r += tok[1]
-                tok = next_token()
-            return r
-        
-        return decode('eof')
-                
+                j = i + 1
+                while j < n and s[j].isalpha():
+                    j += 1
+                q.append(s[i:j])
+                i = j
+        #print(q)
+        return ''.join(q)
             
-            
-        
