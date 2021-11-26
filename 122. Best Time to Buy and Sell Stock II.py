490b56af2789d7f621898d5d27918aa5201cac8f
MATATA <tlxxzj@qq.com>
2021-11-26 14:17:16 +0800

    122. Best Time to Buy and Sell Stock II

--- 122. Best Time to Buy and Sell Stock II.py
+++ 122. Best Time to Buy and Sell Stock II.py
@@ -1,21 +1,9 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
         ret = 0
-        minx = 0
-        maxx = 0
-        if prices:
-            minx=maxx=prices[0]
-        i=0
-        while i < len(prices):
-            if prices[i] > maxx:
-                maxx = prices[i]
-            else:
-                ret += maxx-minx
-                maxx=minx=prices[i]
-            i += 1
-        ret += maxx-minx
+        p = prices[0]
+        for price in prices:
+            if price > p:
+                ret += price - p
+            p = price
         return ret
