a72d501da86b437c7a8aa90c5c87c072c10f76fd
MATATA <tlxxzj@qq.com>
2021-12-08 10:30:53 +0800

    92. Reverse Linked List II

--- 92. Reverse Linked List II.py
+++ 92. Reverse Linked List II.py
@@ -1,38 +1,26 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-class Solution(object):
-    def reverseBetween(self, head, m, n):
-        """
-        :type head: ListNode
-        :type m: int
-        :type n: int
-        :rtype: ListNode
-        """
-        if m == n: return head
-        front, back = None, None
-        k = 1
-        h = head
-        while k != m:
-            front = h
-            h = h.next
-            k += 1
-        #reverse
-        p, q = h, h.next
-        while k != n:
-            x = q.next
-            q.next = p
-            p = q
-            q = x
-            k += 1
-        if front:
-            front.next = p
-        else:
-            head = p
-        h.next = q
-        return head
-
-
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
+        root = ListNode(0, head)
+        i = 0
+        preNode = None
+        node = root
+        l1, l2 = None, None
+        while node:
+            if i == left:
+                l1, l2 = preNode, node
+            elif i == right:
+                l1.next = node
+                l2.next = node.next
+            nextNode = node.next
+            if left < i <= right:
+                node.next = preNode
+            preNode = node
+            node = nextNode
+            i += 1
+        return root.next
+        
