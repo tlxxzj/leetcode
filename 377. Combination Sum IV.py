43c931511d39c589a2ad01281e2b856265826e79
MATATA <tlxxzj@qq.com>
2021-12-09 10:04:36 +0800

    377. Combination Sum IV

--- 377. Combination Sum IV.py
+++ 377. Combination Sum IV.py
@@ -1,15 +1,9 @@
-class Solution(object):
-    def combinationSum4(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
         dp = [0] * (target + 1)
         dp[0] = 1
-        for i in range(target):
+        for i in range(0, target+1):
             for num in nums:
-                if num+i <= target:
-                    dp[num+i] += dp[i]
+                if i+num<=target:
+                    dp[i+num] += dp[i]
         return dp[target]
-        
