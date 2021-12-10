9220e7a7d4c3269d406b034f701364627d76dafd
MATATA <tlxxzj@qq.com>
2021-12-10 17:05:42 +0800

    120. Triangle

--- 120. Triangle.py
+++ 120. Triangle.py
@@ -2,7 +2,7 @@ class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle)
         r1 = [triangle[0][0]]
-        r2 = [inf, inf]
+        r2 = [0, 0]
         for i in range(1, n):
             r2[0] = triangle[i][0] + r1[0]
             r2[-1] = triangle[i][-1] + r1[-1]
