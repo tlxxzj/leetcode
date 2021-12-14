f369b8b4a5318c4cccc2d628ed9e8de244e17228
MATATA <tlxxzj@qq.com>
2021-12-14 15:31:48 +0800

    912. Sort an Array

--- 912. Sort an Array.py
+++ 912. Sort an Array.py
@@ -1,20 +1,24 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        
-        def quick_sort(arr, l, r):
-            if l >= r: return
-            p = arr[l]
-            i, j = l, r
-            while i < j:
-                while i < j and arr[j] >= p:
-                    j -= 1
-                arr[i] = arr[j]
-                while i < j and arr[i] < p:
+        def mergeSort(arr, l, r):
+            if l >= r:
+                return arr[l:r+1]
+            m = (l + r) // 2
+            a1 = mergeSort(arr, l, m)
+            a2 = mergeSort(arr, m+1, r)
+            i, j = 0, 0
+            l1, l2 = len(a1), len(a2)
+            ret = []
+            while i < l1 and j < l2:
+                if a1[i] <= a2[j]:
+                    ret.append(a1[i])
                     i += 1
-                arr[j] = arr[i]
-            arr[i] = p
-            quick_sort(arr, l, i-1)
-            quick_sort(arr, i+1, r)
-        
-        quick_sort(nums, 0, len(nums)-1)
-        return nums
+                else:
+                    ret.append(a2[j])
+                    j += 1
+            if i < l1:
+                ret += a1[i:]
+            elif j < l2:
+                ret += a2[j:]
+            return ret
+        return mergeSort(nums, 0, len(nums)-1)
