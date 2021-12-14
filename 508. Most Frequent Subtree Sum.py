fc266fc0dfca2e84c7539b87ae789e23656cf158
MATATA <tlxxzj@qq.com>
2021-12-14 10:05:06 +0800

    508. Most Frequent Subtree Sum

--- 508. Most Frequent Subtree Sum.py
+++ 508. Most Frequent Subtree Sum.py
@@ -1,32 +1,20 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution(object):
-    def findFrequentTreeSum(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[int]
-        """
-        count = {}
-        def walk(node):
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findFrequentTreeSum(self, root: TreeNode) -> List[int]:
+        from collections import defaultdict
+        freq = defaultdict(int)
+        def find(node):
             if not node:
                 return 0
-            s = node.val + walk(node.left) + walk(node.right)
-            count[s] = count.get(s, 0) + 1
+            s = node.val + find(node.left) + find(node.right)
+            freq[s] += 1
             return s
-        ret = []
-        cnt = -1
-        walk(root)
-        for k, v in count.iteritems():
-            if v > cnt:
-                ret = [k]
-                cnt = v
-            elif v == cnt:
-                ret.append(k)
-        return ret
-            
         
+        find(root)
+        maxFreq = max(freq.values())
+        return [k for k, v in filter(lambda x: x[1] == maxFreq, freq.items())]
