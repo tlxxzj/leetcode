2446c3aaf66df65577f558289b3c3c03a05344cc
MATATA <tlxxzj@qq.com>
2021-12-29 10:50:34 +0800

    96. Unique Binary Search Trees

--- 96. Unique Binary Search Trees.py
+++ 96. Unique Binary Search Trees.py
@@ -1,15 +1,9 @@
-class Solution(object):
-    def numTrees(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        if n < 3:
-            return n
+class Solution:
+    def numTrees(self, n: int) -> int:
         dp = [0] * (n + 1)
-        dp[0] = dp[1] = 1
-        for k in range(2, n+1):
-            for i in range(1, k+1):
-                dp[k] += dp[i-1] * dp[k-i]
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n+1):
+            for j in range(1, i+1):
+                dp[i] += dp[j-1] * dp[i-j]
         return dp[n]
-        
