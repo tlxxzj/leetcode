e549486e3228425d5b8991402c083a781406c97f
MATATA <tlxxzj@qq.com>
2022-01-19 10:10:43 +0800

    743. Network Delay Time

--- 743. Network Delay Time.py
+++ 743. Network Delay Time.py
@@ -1,35 +1,19 @@
 class Solution:
-    def networkDelayTime(self, times, N, K):
-        """
-        :type times: List[List[int]]
-        :type N: int
-        :type K: int
-        :rtype: int
-        """
-        edges = [[] for i in range(N+1)]
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        edge = [[] for i in range(n+1)]
         for u, v, w in times:
-            edges[u].append((v, w))
-
-        d = [-1] * (N + 1)
-        d[0] = 0
-        d[K] = 0
-
-        q = []
-        q.append((K, 0))
+            edge[u].append((v, w))
+        dp = [-1] * (n+1)
+        dp[k] = 0
+        q = [(k, 0)]
         while len(q) > 0:
-            u, w = q.pop()
-            if d[u] != -1 and d[u] < w:
-                continue
-            for v, w2 in edges[u]:
-                w3 = w + w2
-                if d[v] != -1 and d[v] <= w3:
-                    continue
-                d[v] = w3
-                q.append((v, w3))
-
-        minw, maxw = min(d), max(d)
-        if minw == -1:
+            u, t = q.pop()
+            for v, w in edge[u]:
+                if dp[v] == -1 or t + w < dp[v]:
+                    dp[v] = t + w
+                    q.append((v, dp[v]))
+        if dp.count(-1) > 1:
             return -1
-        return maxw
+        return max(dp[1:])
 
-
+            
