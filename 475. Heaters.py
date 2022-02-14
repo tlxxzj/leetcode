260dc51c9db7a6212d1cc9f643771f9c0ba66195
MATATA <tlxxzj@qq.com>
2022-02-14 17:21:50 +0800

    475. Heaters

--- 475. Heaters.py
+++ 475. Heaters.py
@@ -1,34 +1,26 @@
-class Solution(object):
-    def findRadius(self, houses, heaters):
-        """
-        :type houses: List[int]
-        :type heaters: List[int]
-        :rtype: int
-        """
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        houses.sort()
+        heaters.sort()
         
-        def lower_bound(st, left, right, v):
-            while left < right:
-                mid = (left + right)/2
-                if st[mid] < v:
-                    left = mid + 1
+        def binary_search(l, r, val):
+            while l <= r:
+                m = (l + r) >> 1
+                if heaters[m] < val:
+                    l = m + 1
                 else:
-                    right = mid
-            return right
+                    r = m - 1
+            return l
         
         ret = 0
-        heaters.sort()
         for h in houses:
-            x = lower_bound(heaters, 0, len(heaters)-1, h)
-            print x
-            a = abs(h-heaters[x])
-            if x+1<len(heaters):
-                b = abs(h-heaters[x+1])
+            if  h <= heaters[0]:
+                ret = max(ret, heaters[0] - h)
+            elif h >= heaters[-1]:
+                ret = max(ret, h - heaters[-1])
             else:
-                b = a
-            if x-1>=0:
-                c = abs(h-heaters[x-1])
-            else:
-                c = a
-            ret = max(ret, min([a,b,c]))
+                k = binary_search(0, len(heaters)-1, h)
+                x = heaters[k] - h
+                y = h - heaters[k-1]
+                ret = max(ret, min(x, y))
         return ret
-        
