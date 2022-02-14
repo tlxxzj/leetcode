34482ddc154bb27a3ac80a7f35ed3046c24de2a6
MATATA <tlxxzj@qq.com>
2022-02-14 17:33:14 +0800

    475. Heaters

--- 475. Heaters.py
+++ 475. Heaters.py
@@ -1,26 +1,27 @@
 class Solution:
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        houses = houses + heaters
         houses.sort()
-        heaters.sort()
-        
-        def binary_search(l, r, val):
-            while l <= r:
-                m = (l + r) >> 1
-                if heaters[m] < val:
-                    l = m + 1
-                else:
-                    r = m - 1
-            return l
+        heaters = set(heaters)
+        n = len(houses)
+        left, right = [-1] * n, [-1] * n
+        l, r = min(heaters), max(heaters)
+        for i in range(n):
+            if houses[i] in heaters:
+                l = houses[i]
+            else:
+                left[i] = l
+            
+            if houses[-i] in heaters:
+                r = houses[-i]
+            else:
+                right[-i] = r
         
         ret = 0
-        for h in houses:
-            if  h <= heaters[0]:
-                ret = max(ret, heaters[0] - h)
-            elif h >= heaters[-1]:
-                ret = max(ret, h - heaters[-1])
-            else:
-                k = binary_search(0, len(heaters)-1, h)
-                x = heaters[k] - h
-                y = h - heaters[k-1]
-                ret = max(ret, min(x, y))
+        for i in range(n):
+            if houses[i] in heaters:
+                continue
+            h = houses[i]
+            x, y = abs(h - left[i]), abs(h-right[i])
+            ret = max(ret, min(x, y))
         return ret
