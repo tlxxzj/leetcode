c46108a2f868f47d9192bbc25a1afab07d91de0d
MATATA <tlxxzj@qq.com>
2022-02-17 13:41:58 +0800

    881. Boats to Save People

---
+++ 881. Boats to Save People.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        n = len(people)
+        if n <= 1:
+            return 1
+        people.sort()
+        ret = 0
+        i, j = 0, n - 1
+        while i <= j:
+            while j > i and people[i] + people[j] > limit:
+                j -= 1
+                ret += 1
+            if j > i and people[i] + people[j] <= limit:
+                j -= 1
+            ret += 1
+            i += 1
+        return ret
+            
+
+            
