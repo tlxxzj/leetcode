fa82fe72d3377b80bd3abcde3deda02b8757001f
MATATA <tlxxzj@qq.com>
2022-02-24 14:57:18 +0800

    95. Unique Binary Search Trees II

--- 95. Unique Binary Search Trees II.py
+++ 95. Unique Binary Search Trees II.py
@@ -1,31 +1,21 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def generateTrees(self, n):
-        """
-        :type n: int
-        :rtype: List[TreeNode]
-        """
-        if n < 1:
-            return []
-        def gen(l, r):
-            nodes = []
+    def generateTrees(self, n: int) -> List[TreeNode]:
+        def generate(l, r):
             if l > r:
                 return [None]
-            for m in range(l, r+1):
-                lnodes = gen(l, m-1)
-                rnodes = gen(m+1, r)
-                for ln in lnodes:
-                    for rn in rnodes:
-                        root = TreeNode(m)
-                        root.left = ln
-                        root.right = rn
-                        nodes.append(root)
-            return nodes
-
-        return gen(1, n)
+            if l == r:
+                return [TreeNode(l)]
+            trees = []
+            for i in range(l, r+1):
+                left, right = generate(l, i-1), generate(i+1, r)
+                trees.extend([TreeNode(i, n1, n2) for n1 in left for n2 in right])
+            return trees
+        
+        return generate(1, n)
+        
