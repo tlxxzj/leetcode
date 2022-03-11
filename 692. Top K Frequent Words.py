39f0fb71ce3b2602c0ee9991f1a9bd9e846cc270
MATATA <tlxxzj@qq.com>
2022-03-11 10:49:07 +0800

    692. Top K Frequent Words

--- 692. Top K Frequent Words.py
+++ 692. Top K Frequent Words.py
@@ -1,13 +1,16 @@
-import collections
-import heapq
-class Solution(object):
-    def topKFrequent(self, words, k):
-        """
-        :type words: List[str]
-        :type k: int
-        :rtype: List[str]
-        """
-        fq = collections.Counter(words)
-        h = [(-f, w) for w, f in fq.items()]
-        heapq.heapify(h)
-        return [heapq.heappop(h)[1] for i in range(k)]
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        from collections import defaultdict
+        from functools import cmp_to_key
+        def cmp(a, b):
+            if a[1] != b[1]:
+                return a[1]-b[1]
+            elif a[0] < b[0]:
+                return 1
+            return -1
+
+        top = defaultdict(int)
+        for w in words:
+            top[w] += 1
+        
+        return [w[0] for w in sorted(top.items(), key=cmp_to_key(cmp), reverse=True)[:k]]
