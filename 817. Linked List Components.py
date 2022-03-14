863f9d7b2ca3636dd056ad439f0b6e568365f08e
MATATA <tlxxzj@qq.com>
2022-03-14 10:32:26 +0800

    817. Linked List Components

--- 817. Linked List Components.py
+++ 817. Linked List Components.py
@@ -1,22 +1,28 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def numComponents(self, head, G):
-        """
-        :type head: ListNode
-        :type G: List[int]
-        :rtype: int
-        """
-        G = set(G)
-        cnt = 0
-        while head:
-            if head.val in G:
-                cnt += 1
-                while head.next and head.next.val in G:
-                    head = head.next
-            head = head.next
-        return cnt
+    def numComponents(self, head: Optional[ListNode], nums: List[int]) -> int:
+        nums = set(nums)
+        nums2 = set()
+        nodes = {}
+        h = head
+        while h:
+            nodes[h.val] = h
+            h = h.next
+        
+        h = head
+        ret = 0
+        while h:
+            num = h.val
+            h = h.next
+            if (num not in nums) or (num in nums2):
+                continue
+            ret += 1
+            node = nodes[num]
+            while node and node.val in nums:
+                nums2.add(node.val)
+                node = node.next
+        return ret
