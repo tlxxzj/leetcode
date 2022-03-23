6355a732da99a514f73061d568a13420dd8683c5
MATATA <tlxxzj@qq.com>
2022-03-23 10:46:51 +0800

    274. H-Index

--- 274. H-Index.py
+++ 274. H-Index.py
@@ -1,29 +1,11 @@
-class Solution(object):
-    def hIndex(self, citations):
-        """
-        :type citations: List[int]
-        :rtype: int
-        """
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
         n = len(citations)
         citations.sort()
-        
-        def lower_bound(st, l, r, v):
-            x=r
-            while l < r:
-                m = (l+r)/2
-                if st[m]>= v:
-                    x= m
-                    r = m
-                else:
-                    l = m + 1
-            return x
-        
-        for h in range(n, 0, -1):
-            k = lower_bound(citations, 0, n, h)
-            print k
-            if n - k >= h:
-                return h
-        return 0
-        
-        
-        
+        ret = 0
+        for i in range(n):
+            if n-i >= citations[i]:
+                ret = max(ret, citations[i])
+            else:
+                ret = max(ret, n-i)
+        return ret
