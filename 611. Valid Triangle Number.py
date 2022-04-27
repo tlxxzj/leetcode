e03fd0401049187bfc99e9826d832595f5df8754
MATATA <tlxxzj@qq.com>
2022-04-27 14:23:37 +0800

    611. Valid Triangle Number

--- 611. Valid Triangle Number.py
+++ 611. Valid Triangle Number.py
@@ -1,49 +1,29 @@
-class Solution(object):
-    def triangleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        
-        # b-a < c < a+b
-        
-        n = len(nums)
-        ret = 0
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
         nums.sort()
-        
-        # c > b - a
-        def lower(l, r, v):
-            x=-1
-            while l <= r:
-                m = (l+r)/2
-                if nums[m]>v:
-                    x=m
-                    r=m-1
+        ret = 0
+        n = len(nums)
+
+        def upper_bound(l, r, x):
+            while l < r:
+                m = (l + r) // 2
+                if nums[m] <= x:
+                    l = m + 1
                 else:
-                    l=m+1
-            return x
-                    
-        # c < a + b
-        def upper(l, r, v):
-            x=-1
-            while l <= r:
-                m = (l+r)/2
-                if nums[m]<v:
-                    x=m
-                    l=m+1
+                    r = m
+            return l
+        
+        def lower_bound(l, r, x):
+            while l < r:
+                m = (l + r) // 2
+                if nums[m] < x:
+                    l = m + 1
                 else:
-                    r=m-1
-            return x
+                    r = m
+            return l
         
-        for i in xrange(n):
-            for j in xrange(i+1, n):
-                lo = lower(j+1, n-1, nums[j]-nums[i])
-                up = upper(j+1, n-1, nums[i]+nums[j])
-                if lo and up > 0:
-                    ret += up-lo+1
-        
+        for i in range(n):
+            for j in range(i+1, n):
+                ret += lower_bound(j+1, n, nums[i]+nums[j]) - (j+1)
+
         return ret
-                
-        
-        
-        
