3692ebe287d9328203df88f453bfdadf772d5d73
MATATA <tlxxzj@qq.com>
2022-05-25 09:27:38 +0800

    28. Implement strStr()

--- 28. Implement strStr().py
+++ 28. Implement strStr().py
@@ -1,41 +1,14 @@
-class Solution(object):
-    def strStr(self, haystack, needle):
-        """
-        :type haystack: str
-        :type needle: str
-        :rtype: int
-        """
-        if (not needle):
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        m, n = len(haystack), len(needle)
+        if n == 0:
             return 0
-        if (not haystack):
+        if n > m:
             return -1
-        
-        def get_next(s):
-            n = len(s)
-            next = [-1] * (n + 1)
-            i, j = -1, 0
-            while j < n:
-                if i == -1 or s[i] == s[j]:
-                    i += 1
-                    j += 1
-                    next[j] = i
-                else:
-                    i = next[i]
-            return next
-        
-        def kmp(s, p, next):
-            sl, pl = len(s), len(p)
-            i, j = 0, 0
-            while i < sl and j < pl:
-                if j == -1 or s[i] == p[j]:
-                    i += 1
-                    j += 1
-                else:
-                    j = next[j]
-            if j == pl:
-                return i - pl
-            else:
-                return -1
-        
-        return kmp(haystack, needle, get_next(needle))
-        
+        for i in range(m-n+1):
+            j = 0
+            while j < n and haystack[i+j] == needle[j]:
+                j += 1
+            if j == n:
+                return i
+        return -1
