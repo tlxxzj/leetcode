56cde31a56dc1c62b9bae4cca9cf17debc3b04da
MATATA <tlxxzj@qq.com>
2022-06-01 09:29:16 +0800

    70. Climbing Stairs

---
+++ 70. Climbing Stairs.py
@@ -0,0 +1,7 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n + 2)
+        dp[0] = 1
+        for i in range(1, n+1):
+            dp[i] += dp[i-1] + dp[i-2]
+        return dp[n]
