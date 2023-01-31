a275cefbb83a2d57aad2b8f43e51c7755b105370
MATATA <tlxxzj@qq.com>
2023-01-31 13:53:29 +0800

    769. Max Chunks To Make Sorted

--- 769. Max Chunks To Make Sorted.py
+++ 769. Max Chunks To Make Sorted.py
@@ -1,20 +1,19 @@
 class Solution:
-    def maxChunksToSorted(self, arr):
-        """
-        :type arr: List[int]
-        :rtype: int
-        """
+    def maxChunksToSorted(self, arr: List[int]) -> int:
         ret = 0
-        start = 0
-        minp, maxp = -1, -1
-        for i, e in enumerate(arr, 0):
-            if minp == -1:
-                minp, maxp = arr[i], arr[i]
-            else:
-                minp = min(minp, e)
-                maxp = max(maxp, e)
-            if minp == start and maxp == i:
-                ret += 1
-                start = i + 1
-                minp, maxp = -1, -1
+        n = len(arr)
+        i = 0
+        while i < n:
+            l = arr[i]
+            r = arr[i]
+            j = i
+            while j < n:
+                l = min(l, arr[j])
+                r = max(r, arr[j])
+                if l == i and r == j:
+                    j += 1
+                    break
+                j += 1
+            i = j
+            ret += 1
         return ret
