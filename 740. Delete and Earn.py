e43b6a9a22fa3e7c52d509d494a4f7d5ac7ee1dd
MATATA <tlxxzj@qq.com>
2023-02-21 09:37:34 +0800

    740. Delete and Earn

--- 740. Delete and Earn.py
+++ 740. Delete and Earn.py
@@ -1,21 +1,22 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        nums.append(0)
-        maxn = max(nums)
-        arr = [0] * (maxn + 1)
-        for i in nums:
-            arr[i] += 1
+        from collections import defaultdict
         
-        dp = [[0, 0] for i in range(maxn + 1)]
-        ret = 0
-        for i in range(1, maxn + 1):
-            #pick i
-            dp[i][1] = dp[i-1][0] + i * arr[i]
-            
-            #else
+        points = defaultdict(int)
+        for num in nums:
+            points[num] += num
+        
+        nums = list(points.keys())
+        nums.sort()
+        n = len(nums)
+        dp = [[0, 0] for i in range(n)]
+        dp[0] = [0, points[nums[0]]]
+
+        for i in range(1, n):
             dp[i][0] = max(dp[i-1][0], dp[i-1][1])
-            
-            ret = max(dp[i][0], dp[i][1])
+            if nums[i-1] + 1 == nums[i]:
+                dp[i][1] = dp[i-1][0] + points[nums[i]]
+            else:
+                dp[i][1] = dp[i][0] + points[nums[i]]
         
-        return ret
-            
+        return max(dp[-1])
