8d8aa538af1d9b8586c912b0b3660a0aec3e9049
MATATA <tlxxzj@qq.com>
2023-03-29 14:57:18 +0800

    427. Construct Quad Tree

--- 427. Construct Quad Tree.py
+++ 427. Construct Quad Tree.py
@@ -1,6 +1,6 @@
 """
 # Definition for a QuadTree node.
-class Node(object):
+class Node:
     def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
         self.val = val
         self.isLeaf = isLeaf
@@ -9,40 +9,30 @@ class Node(object):
         self.bottomLeft = bottomLeft
         self.bottomRight = bottomRight
 """
-class Solution(object):
-    def construct(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: Node
-        """
-        
-        def isLeaf(g, x, y, x2, y2):
-            for i in range(x, x2):
-                for j in range(y, y2):
-                    if g[i][j] != g[x][y]:
-                        return False
-            return True
-        
-        def visit(g, x, y, x2, y2):
-            val = '*'
-            if g[x][y]:
-                val = True
-            else:
-                val = False
-            n = x2 - x
-            if n == 1 or isLeaf(g, x, y, x2, y2):
-                return Node(val, True, None, None, None, None)
-            else:
-                topLeft = visit(g, x, y, (x+x2)/2, (y+y2)/2)
-                topRight = visit(g, x, (y+y2)/2, (x+x2)/2, y2)
-                bottomLeft = visit(g, (x+x2)/2, y, x2, (y+y2)/2)
-                bottomRight = visit(g, (x+x2)/2, (y+y2)/2, x2, y2)
-                return Node(True, False, topLeft, topRight, bottomLeft, bottomRight)
+
+class Solution:
+    def construct(self, grid: List[List[int]]) -> 'Node':
+        def construct_node(x1, y1, x2, y2) -> Node | int:
+            if x2 - x1 == 1 or y2 - y1 == 1:
+                return grid[x1][y1]
+            x0 = (x1 + x2) // 2
+            y0 = (y1 + y2) // 2
+            topLeft = construct_node(x1, y1, x0, y0)
+            topRight = construct_node(x1, y0, x0, y2)
+            bottomLeft = construct_node(x0, y1, x2, y0)
+            bottomRight = construct_node(x0, y0, x2, y2)
+            if topLeft == topRight == bottomLeft == bottomRight:
+                return topLeft
+            
+            nodes = [topLeft, topRight, bottomLeft, bottomRight]
+            for i in range(4):
+                if not isinstance(nodes[i], Node):
+                    nodes[i] = Node(nodes[i], True, None, None, None, None)
+            
+            return Node(1, False, *nodes)
         
         n = len(grid)
-        return visit(grid, 0, 0, n, n)
-                
-                
-        
-        
-        
+        node = construct_node(0, 0, n, n)
+        if not isinstance(node, Node):
+            node = Node(node, True, None, None, None, None)
+        return node
