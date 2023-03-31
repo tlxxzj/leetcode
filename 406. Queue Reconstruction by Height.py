0e6c7fd11133c560375260ceefdecaf1636a4238
MATATA <tlxxzj@qq.com>
2023-03-31 16:16:20 +0800

    406. Queue Reconstruction by Height

--- 406. Queue Reconstruction by Height.py
+++ 406. Queue Reconstruction by Height.py
@@ -1,23 +1,23 @@
-from functools import cmp_to_key
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        from functools import cmp_to_key
+        def cmp(a, b):
+            if a[0] != b[0]:
+                return a[0] - b[0]
+            return a[1] - b[1]
+        
+        people.sort(key=cmp_to_key(cmp))
+        people.reverse()
         n = len(people)
         ret = [0] * n
-        def cmp(a, b):
-            if a[0] == b[0]:
-                return a[1] - b[1]
-            return a[0] - b[0]
-        people.sort(key=cmp_to_key(cmp))
-        #print(people)
-        for p in people:
-            h, k = p[0], p[1]
-            x = -1
+        while len(people) > 0:
+            p = people.pop()
+            h, k = p
+            k += 1
             for i in range(n):
                 if ret[i] == 0 or ret[i][0] >= h:
-                    x += 1
-                if ret[i] == 0 and x == k:
-                    ret[i] = p
+                    k -= 1
+                if k == 0:
                     break
-                
+            ret[i] = p
         return ret
-        
