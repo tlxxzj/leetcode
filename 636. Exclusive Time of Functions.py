8e2dfd3ff3a15c9cb3bbcbf1f32ff9dadd99076c
MATATA <tlxxzj@qq.com>
2023-04-04 14:12:34 +0800

    636. Exclusive Time of Functions

--- 636. Exclusive Time of Functions.py
+++ 636. Exclusive Time of Functions.py
@@ -1,31 +1,18 @@
-class Solution(object):
-    def exclusiveTime(self, n, logs):
-        """
-        :type n: int
-        :type logs: List[str]
-        :rtype: List[int]
-        """
-        ret = []
-        q = []
+class Solution:
+    def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
+        ret = [0] * n
+        funcs = []
+        prev_time = -1
         for log in logs:
-            id, status, time = log.split(':')
-            id = int(id)
-            time = int(time)
-            if len(q) == 0 or q[-1][0] != id or status == 'start':
-                q.append([id, time, 0])
+            id, start, time = log.split(":")
+            id, start, time = int(id), start=="start", int(time)
+            if start:
+                if len(funcs) > 0:
+                    ret[funcs[-1]] += time - prev_time
+                funcs.append(id)
             else:
-                t = time - q[-1][1] - q[-1][2] + 1
-                if len(q) > 1:
-                    q[-2][2] += time - q[-1][1] + 1
-                ret.append([id, t])
-                q.pop()
-        
-        d = {}
-        for x in ret:
-            id, t = x[0], x[1]
-            d[id] = d.get(id, 0) + t
-        ret = []
-        for k in d:
-            ret.append([k, d[k]])
-        ret.sort()
-        return [x[1] for x in ret]
+                time += 1
+                ret[id] += time - prev_time
+                funcs.pop()
+            prev_time = time
+        return ret
