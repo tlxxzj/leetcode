8518603361d4554c42909f0e9a1fd597270c5def
MATATA <tlxxzj@qq.com>
2023-06-02 18:18:28 +0800

    124. Binary Tree Maximum Path Sum

---
+++ 124. Binary Tree Maximum Path Sum.go
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxPathSum(root *TreeNode) int {
+	ret := math.MinInt32
+
+	max := func(a, b int) int {
+		if a < b {
+			return b
+		}
+		return a
+	}
+
+	var dfs func(node *TreeNode) int
+	dfs = func(node *TreeNode) int {
+		if node == nil {
+			return math.MinInt32
+		}
+
+		left, right := node.Val, node.Val
+
+		if node.Left != nil {
+			left = max(left, node.Val+dfs(node.Left))
+		}
+		if node.Right != nil {
+			right = max(right, node.Val+dfs(node.Right))
+		}
+
+		ret = max(ret, left+right-node.Val)
+		return max(left, right)
+	}
+	dfs(root)
+
+	return ret
+}
