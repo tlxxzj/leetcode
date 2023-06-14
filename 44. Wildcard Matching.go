15b7b8e21535df5b1cbf1c177b9e3547ebfb222c
MATATA <tlxxzj@qq.com>
2023-06-14 14:18:55 +0800

    44. Wildcard Matching

--- 44. Wildcard Matching.go
+++ 44. Wildcard Matching.go
@@ -14,7 +14,7 @@ func isMatch(s string, p string) bool {
 			if s[i] == p[j] || p[j] == '?' {
 				dp[i+1][j+1] = dp[i][j]
 			} else if p[j] == '*' {
-				dp[i+1][j+1] = dp[i][j] || dp[i+1][j] || dp[i][j+1]
+				dp[i+1][j+1] = dp[i+1][j] || dp[i][j+1]
 			}
 		}
 	}
