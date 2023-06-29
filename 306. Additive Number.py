d6858dda561b43ca7f25699462f28096638763a6
MATATA <tlxxzj@qq.com>
2023-06-29 10:31:41 +0800

    306. Additive Number

--- 306. Additive Number.py
+++ 306. Additive Number.py
@@ -1,37 +1,24 @@
 class Solution:
-    def startswith(self, s, s2, start=0):
-        l1, l2 = len(s), len(s2)
-        if l1 - start < l2:
-            return False
-        i = 0
-        while i + start < l1 and i < l2 and s[i+start] == s2[i]:
-            i += 1
-        return i == l2
-
-    def is_additive(self, num, i, j):
-        n = len(num)
-        k = -1
-        while j < n:
-            if (num[k+1] == '0' and i-k >1) or (num[i+1] == '0' and j-i>1):
-                return False
-            a, b = int(num[k+1:i+1]), int(num[i+1:j+1])
-            c = a + b
-            cs = str(c)
-            k = j + len(cs)
-            if not self.startswith(num, cs, j+1):
-                return False
-            i, j, k = j, k, i
-            if j == n - 1:
-                break
-        return j == n - 1
-
     def isAdditiveNumber(self, num: str) -> bool:
         n = len(num)
         if n < 3:
             return False
         
-        for i in range(n):
+        def dfs(a, b, i):
+            if i == n:
+                return True
+            
+            c = str(a+b)
+            if num[i:].startswith(c):
+                return dfs(b, a+b, i+len(c))
+            return False
+        
+        for i in range(1, n):
+            if num[0] == '0' and i > 1:
+                break
             for j in range(i+1, n):
-                if self.is_additive(num, i, j):
+                if num[i] == '0' and j-i>1:
+                    break
+                if dfs(int(num[:i]), int(num[i:j]), j):
                     return True
         return False
