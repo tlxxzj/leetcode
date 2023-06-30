0e6187f0ed34a7173eaa718a1c0cd220e374f901
MATATA <tlxxzj@qq.com>
2023-06-30 11:46:57 +0800

    97. Interleaving String

--- 97. Interleaving String.py
+++ 97. Interleaving String.py
@@ -1,25 +1,14 @@
-class Solution(object):
-    def isInterleave(self, s1, s2, s3):
-        """
-        :type s1: str
-        :type s2: str
-        :type s3: str
-        :rtype: bool
-        """
-        l1, l2 ,l3 = len(s1), len(s2), len(s3)
-        if l1 + l2 != l3:
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        n1, n2 ,n3 = len(s1), len(s2), len(s3)
+        if n1 + n2 != n3:
             return False
-        dp = [[False] * (l2 + 1) for i in range(l1 + 1)]
+        dp = [[False]*(n2+1) for _ in range(n1+1)]
         dp[0][0] = True
-        for i in xrange(l1+1):
-            for j in xrange(l2+1):
-                k = i + j
-                if dp[i][j]:
-                    if i<l1 and s1[i] == s3[k]:
-                        dp[i+1][j] = True
-                    if j<l2 and s2[j] == s3[k]:
-                        dp[i][j+1] = True
-        
-        return dp[l1][l2]
-        
-        
+        for i in range(n1+1):
+            for j in range(n2+1):
+                if i > 0:
+                    dp[i][j] |= dp[i-1][j] and s1[i-1] == s3[i+j-1]
+                if j > 0:
+                    dp[i][j] |= dp[i][j-1] and s2[j-1] == s3[i+j-1]
+        return dp[n1][n2]
