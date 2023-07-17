1526fe04911c3c47e1a821a06febf30b1f58b6f4
MATATA <tlxxzj@qq.com>
2023-07-17 15:10:28 +0800

    695. Max Area of Island

--- 695. Max Area of Island.py
+++ 695. Max Area of Island.py
@@ -1,31 +1,20 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        if not grid or len(grid) == 0 or len(grid[0]) == 0:
-            return 0
-        n, m = len(grid), len(grid[0])
-        ret = 0
-        island = lambda x, y: x>=0 and x<n and y>=0 and y<m and grid[x][y] == 1
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == 1:
-                    grid[i][j] = 0
-                    q = [[i, j]]
-                    area = 0
-                    while len(q) > 0:
-                        area += 1
-                        x, y = q.pop()
-                        if island(x-1, y):
-                            q.append([x-1, y])
-                            grid[x-1][y] = 0
-                        if island(x+1, y):
-                            q.append([x+1, y])
-                            grid[x+1][y] = 0
-                        if island(x, y-1):
-                            q.append([x, y-1])
-                            grid[x][y-1] = 0
-                        if island(x, y+1):
-                            q.append([x, y+1])
-                            grid[x][y+1] = 0
-                    ret = max(ret, area)
+        res = 0
+        m, n = len(grid), len(grid[0])
+        visited = [[False] * n for _ in range(m)]
+
+        def dfs(x, y):
+            if x < 0 or x >= m or y < 0 or y >= n:
+                return 0
+            if grid[x][y] == 0 or visited[x][y]:
+                return 0
+            visited[x][y] = True
+            return dfs(x+1, y) + dfs(x-1, y) + dfs(x, y+1) + dfs(x, y-1) + 1
+
+        for i in range(m):
+            for j in range(n):
+                area = dfs(i, j)
+                res = max(res, area)
         
-        return ret
+        return res
