25546db41e87491dbc98b401b863f358168f2027
MATATA <tlxxzj@qq.com>
2023-07-27 14:56:55 +0800

    509. Fibonacci Number

--- 509. Fibonacci Number.py
+++ 509. Fibonacci Number.py
@@ -1,11 +1,10 @@
 class Solution:
-    def fib(self, N: 'int') -> 'int':
-        if N <= 1:
-            return N
-        f = [0]*(N + 1)
-        f[0] = 0
-        f[1] = 1
-        for i in range(2, N + 1):
-            f[i] = f[i-1] + f[i-2]
-        return f[N]
+    def fib(self, n: int) -> int:
+        if n <= 1:
+            return n
         
+        a, b = 0, 1
+        for i in range(n-1):
+            a, b = b, a+b
+        
+        return b
