a8cff4d2b23dcc6336e55d076bd4092620c53ac1
MATATA <tlxxzj@163.com>
2023-08-15 14:03:34 +0800

    Update 551. Student Attendance Record I.py

--- 551. Student Attendance Record I.py
+++ 551. Student Attendance Record I.py
@@ -1,15 +1,17 @@
-class Solution(object):
-    def checkRecord(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        cnt=0
-        last=0
-        for c in s:
-            if c == 'A':
-                cnt += 1
-                if cnt > 1:
+class Solution:
+    def checkRecord(self, s: str) -> bool:
+        absent = 0
+        late = 0
+        for r in s:
+            if r == 'A':
+                late = 0
+                absent += 1
+                if absent >= 2:
                     return False
-        return 'LLL' not in s
-        
+            elif r == 'L':
+                late += 1
+                if late >= 3:
+                    return False
+            else:
+                late = 0
+        return True
