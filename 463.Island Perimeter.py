8471ea2d60f04e17dc0f0a58448817f84716eafa
MATATA <tlxxzj@163.com>
2023-08-18 15:17:21 +0800

    Update 463.Island Perimeter.py

--- 463.Island Perimeter.py
+++ 463.Island Perimeter.py
@@ -1,22 +1,20 @@
-class Solution(object):
-    def islandPerimeter(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        if not grid or not grid[0]: return 0
-        m,n = len(grid), len(grid[0])
-        ret = 0
-        def isEdge(i, j):
-            if i<0 or i >=m or j<0 or j>=n or grid[i][j] ==0:
-                return 1
-            return 0
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        res = 0
+
+        def neighbors(x, y):
+            cnt = 0
+            for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+                x2, y2 = x+dx, y+dy
+                if x2>=0 and x2<m and y2>=0 and y2<n and grid[x2][y2] == 1:
+                    cnt += 1
+            return cnt
+
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == 1:
-                    ret += isEdge(i+1, j) + isEdge(i-1, j) + isEdge(i, j+1) + isEdge(i, j-1)
-        return ret
+                if grid[i][j] == 0:
+                    continue
+                res += 4 - neighbors(i, j)
         
-        
-        
-        
+        return res
