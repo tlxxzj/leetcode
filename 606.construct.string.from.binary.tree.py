f28e73e938ea0339900830c87fd924b9c336f841
MATATA <tlxxzj@163.com>
2023-09-06 11:10:34 +0800

    Update 606.construct.string.from.binary.tree.py

--- 606.construct.string.from.binary.tree.py
+++ 606.construct.string.from.binary.tree.py
@@ -1,21 +1,21 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        if not root:
+            return ""
+        val = str(root.val)
+        left = self.tree2str(root.left)
+        right = self.tree2str(root.right)
 
-class Solution(object):
-    def tree2str(self, t):
-        """
-        :type t: TreeNode
-        :rtype: str
-        """
-        if t is None: return ''
-        if t.left is None and t.right is None:
-            return '%s' %(str(t.val))
-        if t.left is None:
-            return '%s()(%s)' %(str(t.val), self.tree2str(t.right))
-        if t.right is None:
-            return '%s(%s)' %(str(t.val), self.tree2str(t.left))
-        return '%s(%s)(%s)' %(str(t.val), self.tree2str(t.left), self.tree2str(t.right))
+        if left == "" and right == "":
+            return val
+        
+        if right == "":
+            return f"{val}({left})"
+        
+        return f"{val}({left})({right})"
