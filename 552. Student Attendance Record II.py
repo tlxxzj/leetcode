309a270d73c46e75215fcbb6c7a4ef47a44c1dec
MATATA <tlxxzj@163.com>
2023-09-26 13:21:04 +0800

    Update 552. Student Attendance Record II.py

--- 552. Student Attendance Record II.py
+++ 552. Student Attendance Record II.py
@@ -1,34 +1,26 @@
-class Solution(object):
-    def checkRecord(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        mod = 1000000007
-        class Node:
-            def __init__(self):
-                self.a0=0
-                self.a1=0
-                self.a2=0
-                self.l0=0
-                self.l1=0
-                self.l2=0
-        a, b = Node(), Node()
-        a.l0=1
-        for i in xrange(1, n+1):
-            a, b = b, a
-            a.l0 = (b.l0 + b.l1 + b.l2) % mod
-            a.l1 = b.l0
-            a.l2 = b.l1
-            
-            a.a0 = (b.a0 + b.a1 + b.a2 + b.l0 + b.l1 + b.l2) % mod
-            a.a1 = b.a0
-            a.a2 = b.a1
+class Solution:
+    def checkRecord(self, n: int) -> int:
+        MOD = 10**9 + 7
+        A = 0
+        L = 1
+        LL = 2
+        P = 3
+        AL = 4
+        ALL = 5
+        AP = 6
+
+        dp = [[0] * 7 for _ in range(n)]
+        dp[0] = [1,1,0,1,0,0,0]
+
+        for i in range(1, n):
+            dp[i][A] = (dp[i-1][L] + dp[i-1][LL] +dp[i-1][P]) % MOD
+            dp[i][L] = dp[i-1][P]
+            dp[i][LL] = dp[i-1][L]
+            dp[i][P] = (dp[i-1][L] + dp[i-1][LL] + dp[i-1][P]) % MOD
+
+            dp[i][AL] = (dp[i-1][A] + dp[i-1][AP]) % MOD
+            dp[i][ALL] = dp[i-1][AL]
+            dp[i][AP] = (dp[i-1][A] + dp[i-1][AL] + dp[i-1][ALL] + dp[i-1][AP]) % MOD
         
-        x=a
-        return (x.a0 + x.a1 + x.a2 + x.l0 + x.l1 + x.l2) % mod
-            
-            
-            
-            
-            
+        return sum(dp[n-1]) % MOD
+
