d25a137bffd4a2e47e18625ef199f79a7b30bd4b
MATATA <tlxxzj@163.com>
2023-10-19 14:34:10 +0800

    Update 309. Best Time to Buy and Sell Stock with Cooldown.py

--- 309. Best Time to Buy and Sell Stock with Cooldown.py
+++ 309. Best Time to Buy and Sell Stock with Cooldown.py
@@ -7,13 +7,14 @@ class Solution(object):
         n = len(prices)
         if n < 2:
             return 0
-        dp = [0]*n
-        for i in range(0, n-1):
-            for j in range(i+1, n):
-                if prices[j] > prices[i]:
-                    x = dp[i-2] if i-2>=0 else 0
-                    dp[j] = max(dp[j], x+prices[j]-prices[i])
-                else:
-                    dp[j] = max(dp[j], dp[j-1])
-        return dp[n-1]
 
+        dp = [[0]*3 for i in range(n)]
+        dp[0][0] = -prices[0]
+        
+        maxprofit = 0
+        for i in range(1, n):
+            dp[i][0] = max(dp[i-1][2] - prices[i], dp[i-1][0])
+            dp[i][1] = max(prices[i] + dp[i-1][0], dp[i-1][1])
+            dp[i][2] = dp[i-1][1]
+            maxprofit = max(maxprofit, dp[i][1])
+        return maxprofit
