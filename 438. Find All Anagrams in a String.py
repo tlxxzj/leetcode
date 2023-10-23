425009e4364e7f56724422c6a7e44825f667aca5
MATATA <tlxxzj@163.com>
2023-10-23 13:54:21 +0800

    Update 438. Find All Anagrams in a String.py

--- 438. Find All Anagrams in a String.py
+++ 438. Find All Anagrams in a String.py
@@ -1,57 +1,25 @@
-class Solution(object):
-    def findAnagrams(self, s, p):
-        """
-        :type s: str
-        :type p: str
-        :rtype: List[int]
-        """
-        oa = ord('a')
-        def c2x(c):
-            return ord(c)-oa
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        def c2i(c):
+            return ord(c)-ord('a')
         
-        sl = len(s)
-        pl = len(p)
-        st = [0] * 26
+        m, n = len(s), len(p)
+        count = [0] * 26
         for c in p:
-            st[c2x(c)] += 1
+            count[c2i(c)] += 1
         
-        nz = 0
-        for i in st:
-            if i:
-                nz += 1
-        
-        for c in s[:pl-1]:
-            x=c2x(c)
-            if st[x] ==0:
-                st[x] -= 1
-                nz += 1
-            else:
-                st[x] -= 1
-                if st[x] == 0:
-                    nz -= 1
-        ret = []
-        i = pl-1
-        for i in range(pl-1, sl):
-            c = s[i]
-            x=c2x(c)
-            if st[x] == 0:
-                st[x] -= 1
-                nz +=1
-            else:
-                st[x] -= 1
-                if st[x] ==0:
-                    nz -= 1
-            if nz ==0 :
-                ret.append(i-pl+1)
+        res = []
+        l = 0
+        for r in range(m):
+            i = c2i(s[r])
+            count[i] -= 1
             
-            x = c2x(s[i-pl+1])
-            if st[x] == 0:
-                st[x] += 1
-                nz += 1
-            else:
-                st[x] += 1
-                if st[x] == 0:
-                    nz -= 1
-        
-        return ret
-        
+            while count[i] < 0:
+                count[c2i(s[l])] += 1
+                l += 1
+            
+            if r-l+1 == n:
+                res.append(l)
+                count[c2i(s[l])] += 1
+                l += 1
+        return res
