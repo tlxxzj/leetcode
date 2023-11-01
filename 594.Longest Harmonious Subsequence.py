abfb4b50aaa0b187c84bdfd037fe804eb35cd7bc
MATATA <tlxxzj@163.com>
2023-11-01 10:48:01 +0800

    Update 594.Longest Harmonious Subsequence.py

--- 594.Longest Harmonious Subsequence.py
+++ 594.Longest Harmonious Subsequence.py
@@ -1,17 +1,13 @@
-class Solution(object):
-    def findLHS(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        d = {}
-        for i in nums:
-            d[i] = d.get(i, 0) + 1
-        ret = 0
-        for k, v in d.iteritems():
-            if k-1 in d:
-                ret = max(ret, v+d[k-1])
-            if k+1 in d:
-                ret = max(ret, v+d[k+1])
-        return ret
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        cnt = {}
+        for num in nums:
+            cnt[num] = cnt.get(num, 0) + 1
         
+        ret = 0
+        pre = None
+        for num in sorted(cnt.keys()):
+            if pre != None and pre + 1 == num:
+                ret = max(ret, cnt[pre] + cnt[num])
+            pre = num
+        return ret
