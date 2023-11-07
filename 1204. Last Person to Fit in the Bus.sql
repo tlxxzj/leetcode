3f0ebb6c9bcd53fd9315a56ebd8579be34ab19fa
MATATA <tlxxzj@163.com>
2023-11-07 11:09:41 +0800

    Create 1204. Last Person to Fit in the Bus.sql

---
+++ 1204. Last Person to Fit in the Bus.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select a.person_name
+from Queue a, Queue b
+where a.turn >= b.turn
+group by a.person_id
+having sum(b.weight) <= 1000
+order by a.turn desc
+limit 1
