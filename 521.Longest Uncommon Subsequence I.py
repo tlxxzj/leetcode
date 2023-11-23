9a4c8b83134d5580d4c6565d662f7bbfe2318744
MATATA <tlxxzj@163.com>
2023-11-23 15:37:50 +0800

    Update 521.Longest Uncommon Subsequence I.py

--- 521.Longest Uncommon Subsequence I.py
+++ 521.Longest Uncommon Subsequence I.py
@@ -1,14 +1,5 @@
-class Solution(object):
-    def findLUSlength(self, a, b):
-        """
-        :type a: str
-        :type b: str
-        :rtype: int
-        """
-        l1, l2 = len(a), len(b)
-        if l1 != l2:
-            return max(l1, l2)
-        if a != b:
-            return l1
-        return -1
-        
+class Solution:
+    def findLUSlength(self, a: str, b: str) -> int:
+        if a == b:
+            return -1
+        return max(len(a), len(b))
