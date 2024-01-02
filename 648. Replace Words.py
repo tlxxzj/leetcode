42b5a95ffe366fadd7aa5a13da91c6166985e121
MATATA <tlxxzj@163.com>
2024-01-02 15:51:34 +0800

    Update 648. Replace Words.py

--- 648. Replace Words.py
+++ 648. Replace Words.py
@@ -1,12 +1,35 @@
 class Solution:
-    def replaceWords(self, dict: List[str], sentence: str) -> str:
-        ret = []
-        dict.sort(key=lambda x: len(x))
-        for word in sentence.split(' '):
-            r = word
-            for root in dict:
-                if word.startswith(root):
-                    r = root
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        root = {}
+        for word in dictionary:
+            node = root
+            for c in word:
+                if c not in node:
+                    node[c] = {}
+                node = node[c]
+            node["root"] = True
+        
+        def getRoot(word):
+            chars = []
+            node = root
+            for c in word:
+                if "root" in node:
                     break
-            ret.append(r)
-        return ' '.join(ret)
+                if c not in node:
+                    return ""
+                node = node[c]
+                chars.append(c)
+            if "root" in node:
+                return "".join(chars)
+            return ""
+        
+        res = []
+        for word in sentence.split(" "):
+            r = getRoot(word)
+            print(word, r)
+            if r == "":
+                res.append(word)
+            else:
+                res.append(r)
+        
+        return " ".join(res)
